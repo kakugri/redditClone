3a5f4ebf4d3d336179b676488eeb8878bfe8089d
kakugri <akugri95@gmail.com>
2024-12-13 20:33:09 -0500

    Added all functionalities to the clients simulator

--- cmd/clients/clients.go
+++ cmd/clients/clients.go
@@ -4,23 +4,27 @@ import (
 	"bytes"
 	"encoding/json"
 	"log"
+	"math/rand"
 	"net/http"
 	"strconv"
 	"sync"
 )
 
-const apiURL = "http://localhost:8081/api/register"
+const registerURL = "http://localhost:8081/api/register"
+const postURL = "http://localhost:8081/api/posts"
+const subredditURL = "http://localhost:8081/api/newsubreddit"
+const commentURL = "http://localhost:8081/api/comment"
 
 func main() {
 	var wg sync.WaitGroup
-	clientCount := 100
+	clientCount := 100000
 
 	for i := 0; i < clientCount; i++ {
 		wg.Add(1)
 		go func(clientID int) {
 			defer wg.Done()
 			username := "user" + strconv.Itoa(clientID)
-			registerUser(username)
+			simulateRequest(username)
 		}(i)
 	}
 
@@ -28,15 +32,56 @@ func main() {
 	log.Println("All clients finished")
 }
 
-func registerUser(username string) {
-	data := map[string]string{"username": username}
-	body, _ := json.Marshal(data)
+func simulateRequest(username string) {
+	switch rand.Intn(4) {
+	case 0:
+		data := map[string]string{"username": username}
+		body, _ := json.Marshal(data)
 
-	resp, err := http.Post(apiURL, "application/json", bytes.NewBuffer(body))
-	if err != nil {
-		log.Printf("Error registering user %s: %v", username, err)
-		return
+		resp, err := http.Post(registerURL, "application/json", bytes.NewBuffer(body))
+		if err != nil {
+			log.Printf("Error registering user %s: %v", username, err)
+			return
+		}
+		defer resp.Body.Close()
+		log.Printf("Registered user: %s, Status: %d", username, resp.StatusCode)
+	case 1:
+		title := "Simulated Post Title"
+		content := "Simulated Post Content"
+		data := map[string]string{"title": title, "content": content}
+		body, _ := json.Marshal(data)
+
+		resp, err := http.Post(postURL, "application/json", bytes.NewBuffer(body))
+		if err != nil {
+			log.Printf("Error sending post with title %s: %v", title, err)
+			return
+		}
+		defer resp.Body.Close()
+		log.Printf("Sent post by user: %s, Status: %d", username, resp.StatusCode)
+	case 2:
+		description := "Simulated Subreddit Description"
+		creatorid := username
+		data := map[string]string{"username": username, "description": description, "creatorid": creatorid}
+		body, _ := json.Marshal(data)
+
+		resp, err := http.Post(subredditURL, "application/json", bytes.NewBuffer(body))
+		if err != nil {
+			log.Printf("Error creating subreddit with description %s: %v", description, err)
+			return
+		}
+		defer resp.Body.Close()
+		log.Printf("Created subreddit by user: %s, Status: %d", username, resp.StatusCode)
+	case 3:
+		content := "Simulated Comment Content"
+		data := map[string]string{"content": content}
+		body, _ := json.Marshal(data)
+
+		resp, err := http.Post(commentURL, "application/json", bytes.NewBuffer(body))
+		if err != nil {
+			log.Printf("Error creating comment by user %s: %v", username, err)
+			return
+		}
+		defer resp.Body.Close()
+		log.Printf("Created comment by user: %s, Status: %d", username, resp.StatusCode)
 	}
-	defer resp.Body.Close()
-	log.Printf("Registered user: %s, Status: %d", username, resp.StatusCode)
 }
